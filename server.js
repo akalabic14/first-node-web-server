4885f06a7cc35f019e50592107f23e347a18df92
= <=>
2017-02-25 22:46:02 +0100

    Added starting script and port constant for deploy

--- server.js
+++ server.js
@@ -2,27 +2,34 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000 ;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set( 'view engine', 'hbs' );
-app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
+app.listen(port);
+
 app.use(( req, res, next ) => {
   var now = new Date().toString();
   var log  = `${now}: ${req.method} : ${req.url}`;
-  fs.appendFile('server.log',log + '/n');
+  fs.appendFile('server.log',log + "/n");
   next();
 });
 
-app.use(( req, res, next ) => {
-  res.render('whilewait.hbs');
-})
+// This is being used when we need to do something
+// on the server and we don't want anyone to access
+// any of our pages
 
-app.listen(3000);
+// app.use(( req, res, next ) => {
+//   res.render('whilewait.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', ( req, res ) => {
   res.render('home.hbs',{
